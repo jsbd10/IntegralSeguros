e884c63e56bbcc9cdadbf52e3f62355b70cfc48c
santiagorestrepoo09 <dsrestrepo04@ucatolica.edu.co>
2022-02-19 17:00:00 -0500

    To add bootstrap

--- integralSeguros/src/app/app.module.ts
+++ integralSeguros/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
